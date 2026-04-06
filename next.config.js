0e3d66b31b973e03e445efb38c50e4e72ab1ede3
Vishal Singh <singh.vishal3066@gmail.com>
2026-04-07 00:39:44 +0530

    added export to make it live

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: isGithubPages ? "/me" : "",
+  assetPrefix: isGithubPages ? "/me/" : undefined,
   images: {
     unoptimized: true
   }
